04bd08cfff6ffd25f177c83d21e92386281f005b
panurg <panurg@bk.ru>
2013-12-11 19:16:51 +0400

    completed exercise with calculating velocity

---
+++ operators.04/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        float distance = 0.666f;
+        float time = 0.42f;
+        float velocity = distance / time;
+        System.out.println("Velocity is " + velocity);
+    }
+}
