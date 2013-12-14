1b19d2bc2c5fb5b9b12f74d5829be73542102860
panurg <panurg@bk.ru>
2013-12-14 23:35:55 +0400

    completed exercise with float and double exponential notation

---
+++ operators.09/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Float min: " + Float.MIN_VALUE);
+        System.out.println("Float max: " + Float.MAX_VALUE);
+        System.out.println("Double min: " + Double.MIN_VALUE);
+        System.out.println("Double max: " + Double.MAX_VALUE);
+    }
+}
