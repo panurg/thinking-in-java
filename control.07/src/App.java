bacf7f92c4b4d4ead78e988694498539da4db857
panurg <panurg@bk.ru>
2014-01-27 18:31:11 +0400

    completed exercise with break and return statements

---
+++ control.07/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i == 99)
+                break;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+
