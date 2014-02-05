2d7ee9820cc993a5794f4b47a3f4f5510409fa72
panurg <panurg@bk.ru>
2014-02-05 18:51:17 +0400

    completed exercise in which libaray public class is demonstrated

---
+++ access.07/src/App.java
@@ -0,0 +1,7 @@
+import access.*;
+
+public class App {
+    public static void main(String[] args) {
+        Widget widget = new Widget();
+    }
+}
---
+++ access.07/src/access/Widget.java
@@ -0,0 +1,7 @@
+package access;
+
+public class Widget {
+    public Widget() {
+        System.out.println("Widget created");
+    }
+}
