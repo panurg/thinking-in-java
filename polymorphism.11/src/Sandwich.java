dc621b3e0a4fd1c9cc69798c78a7a0fa67d2c10e
panurg <panurg@bk.ru>
2014-03-03 18:34:23 +0400

    completed exercise in which constructors call orded is showed

---
+++ polymorphism.11/src/Sandwich.java
@@ -0,0 +1,46 @@
+/*
+ * (1) Add class Pickle to Sandwich.java.
+ */
+
+// package polymorphism;
+import static net.mindview.util.Print.*;
+
+class Meal {
+    Meal() { print("Meal()"); }
+}
+
+class Bread {
+    Bread() { print("Bread()"); }
+}
+
+class Cheese {
+    Cheese() { print("Cheese()"); }
+}
+
+class Lettuce {
+    Lettuce() { print("Lettuce()"); }
+}
+
+class Pickle {
+    Pickle() { print("Pickle()"); }
+}
+
+class Lunch extends Meal {
+    Lunch() { print("Lunch()"); }
+}
+
+class PortableLunch extends Lunch {
+    PortableLunch() { print("PortableLunch()");}
+}
+
+public class Sandwich extends PortableLunch {
+    private Bread b = new Bread();
+    private Cheese c = new Cheese();
+    private Lettuce l = new Lettuce();
+    private Pickle p = new Pickle();
+    public Sandwich() { print("Sandwich()"); }
+    public static void main(String[] args) {
+        new Sandwich();
+    }
+}
+
